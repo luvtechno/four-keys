428686bb5a682763ac02afa36626381a9fdab022
Yoshinori Kawasaki <ykawasaki@gmail.com>
2023-12-14 10:50:02 +0900

    Support multiple repos

--- lead-time/lead-time.rb
+++ lead-time/lead-time.rb
@@ -9,18 +9,12 @@ require_relative 'github'
 
 repo_root_dir = ENV['REPO_ROOT_DIR']
 repo_owner = ENV['REPO_OWNER']
-repo_name = ENV['REPO_NAME']
-repo_dir = "#{repo_root_dir}/#{repo_owner}/#{repo_name}"
+repo_names = ENV['REPO_NAME'].split(',')
 production_branch_name = ENV['PRODUCTION_BRANCH_NAME']
 
-repo = Repo.new(repo_dir: repo_dir, production_branch_name: production_branch_name)
-repo.load_deployment_times
-
-github = GitHub.new(repo_owner: repo_owner, repo_name: repo_name)
-pull_requests = github.load_pull_requests(debug: false)
-
-output_file = "#{repo_name}.csv"
+output_file = "#{repo_owner}.csv"
 headers = [
+  'repository',
   '#',
   'URL',
   'title',
@@ -33,24 +27,35 @@ headers = [
   'leadTime',
 ]
 CSV.open(output_file, 'w', headers: headers, write_headers: true) do |csv|
-  pull_requests.each do |pr|
-    created_at = Time.parse(pr.created_at).getlocal
-    merged_at = Time.parse(pr.merged_at).getlocal
-    deployed_at = repo.deployment_time(oid: pr.merge_commit.oid)
-    lead_time = deployed_at ? (deployed_at - created_at) / (60 * 60 * 24) : nil
+  repo_names.each do |repo_name|
+    repo_dir = "#{repo_root_dir}/#{repo_owner}/#{repo_name}"
 
-    csv << [
-      pr.number,
-      pr.permalink,
-      pr.title,
-      pr.author.login,
-      pr.base_ref_name,
-      created_at.strftime("%Y-%m-%d %H:%M:%S"),
-      merged_at&.strftime("%Y-%m-%d %H:%M:%S"),
-      pr.merge_commit.oid,
-      deployed_at&.strftime("%Y-%m-%d %H:%M:%S"),
-      lead_time,
-    ]
+    repo = Repo.new(repo_dir: repo_dir, production_branch_name: production_branch_name)
+    repo.load_deployment_times
+
+    github = GitHub.new(repo_owner: repo_owner, repo_name: repo_name)
+    pull_requests = github.load_pull_requests(debug: false)
+
+    pull_requests.each do |pr|
+      created_at = Time.parse(pr.created_at).getlocal
+      merged_at = Time.parse(pr.merged_at).getlocal
+      deployed_at = repo.deployment_time(oid: pr.merge_commit.oid)
+      lead_time = deployed_at ? (deployed_at - created_at) / (60 * 60 * 24) : nil
+
+      csv << [
+        repo_name,
+        pr.number,
+        pr.permalink,
+        pr.title,
+        pr.author.login,
+        pr.base_ref_name,
+        created_at.strftime("%Y-%m-%d %H:%M:%S"),
+        merged_at&.strftime("%Y-%m-%d %H:%M:%S"),
+        pr.merge_commit.oid,
+        deployed_at&.strftime("%Y-%m-%d %H:%M:%S"),
+        lead_time,
+      ]
+    end
   end
 end
 
